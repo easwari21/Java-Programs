eeac47dd361ca13fbf459a3a48bef6ac4b702131
easwari21 <you@exampl.com>
2025-05-10 12:54:13 +0530

    save

--- programs/Adt/absDatatypes.java
+++
@@ -1,8 +0,0 @@
-public class absDatatypes {
-    public static void main(String[] args) {
-        //get,remove,add,getsize,rotate,shift
-        
-        int [] arr = new int[20];
-        
-    }
-}
